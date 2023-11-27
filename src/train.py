d2e99ca5673e058e376b2c7dd6d6e6482f1a7a87
Hanna-Gao <qg2205@instance-3.c.hpml-1.internal>
2023-11-27 01:19:54 +0000

    Midpoint1

--- src/train.py
+++ src/train.py
@@ -7,6 +7,10 @@ import csv
 from PIL import Image
 from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
+import time
+import os
+import argparse
+# from torch.profiler import profile, ProfilerActivity
 
 if not torch.cuda.is_available():
     from torchsummary import summary
@@ -80,6 +84,17 @@ class DataSet(torch.utils.data.Dataset):
 
 
 def main():
+
+    parser = argparse.ArgumentParser(description='Facial Expression Recognition')
+    # parser.add_argument('--cuda', action='store_true', help='Set to use Cuda')
+    # parser.add_argument('--path', default='./data', type=str, help='Data Path')
+    parser.add_argument('--n', default=1, type=int, help='number of dataloader workers')
+    # parser.add_argument('--optimizer', '-o', default='sgd', type=str, help='the optimizer (as string, eg: ‘sgd’)')
+    args = parser.parse_args()
+
+    for arg, value in vars(args).items():
+        print(arg, ":", value)
+
     # variables  -------------
     batch_size = 128
     lr = 0.01
@@ -88,6 +103,11 @@ def main():
     learning_rate_decay_every = 5
     learning_rate_decay_rate = 0.9
     # ------------------------
+
+    data_loading_time = []
+    training_time = []
+    total_epoch_time = []
+
 
     classes = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
     network = model.Model(num_classes=len(classes)).to(device)
@@ -98,10 +118,10 @@ def main():
     criterion = nn.CrossEntropyLoss()
     factory = DataSetFactory()
 
-    training_loader = DataLoader(factory.training, batch_size=batch_size, shuffle=True, num_workers=1)
+    training_loader = DataLoader(factory.training, batch_size=batch_size, shuffle=True, num_workers=args.n)
     validation_loader = {
-        'private': DataLoader(factory.private, batch_size=batch_size, shuffle=True, num_workers=1),
-        'public': DataLoader(factory.public, batch_size=batch_size, shuffle=True, num_workers=1)
+        'private': DataLoader(factory.private, batch_size=batch_size, shuffle=True, num_workers=args.n),
+        'public': DataLoader(factory.public, batch_size=batch_size, shuffle=True, num_workers=args.n)
     }
 
     min_validation_loss = {
@@ -110,6 +130,9 @@ def main():
     }
 
     for epoch in range(epochs):
+        # Profiliing time for each epoch
+        epoch_start = time.perf_counter()
+
         network.train()
         total = 0
         correct = 0
@@ -126,21 +149,58 @@ def main():
             current_lr = lr
 
         print('learning_rate: %s' % str(current_lr))
-        for i, (x_train, y_train) in enumerate(training_loader):
-            optimizer.zero_grad()
+
+        # with profile(activities=[
+        #     ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True, profile_memory=True) as prof:
+
+        it = iter(training_loader)
+        total_batch = len(training_loader)
+        batch_idx = 0
+        total_train_time = 0
+        total_dataload_time = 0
+
+        for i in range(total_batch):
+        # for i, (x_train, y_train) in enumerate(training_loader):
+            
+
+            # Profiling data loading time
+            start = time.perf_counter()
+            x_train, y_train = next(it)
+            end = time.perf_counter()
+            total_dataload_time += end - start
+            
             x_train = x_train.to(device)
             y_train = y_train.to(device)
+
+            # Profiling training time
+            start = time.perf_counter()
+            optimizer.zero_grad()
             y_predicted = network(x_train)
             loss = criterion(y_predicted, y_train)
             loss.backward()
             optimizer.step()
+            end = time.perf_counter()
+            total_train_time += end - start
+
             _, predicted = torch.max(y_predicted.data, 1)
             total_train_loss += loss.data
             total += y_train.size(0)
             correct += predicted.eq(y_train.data).sum()
+
+        epoch_end = time.perf_counter()
+        epoch_time = epoch_end - epoch_start
+
         accuracy = 100. * float(correct) / total
+        print('----------------------------------------------------------------------')
         print('Epoch [%d/%d] Training Loss: %.4f, Accuracy: %.4f' % (
             epoch + 1, epochs, total_train_loss / (i + 1), accuracy))
+        print('Total DataLoading Time: {} | Total Training Time: {} | Total Time of Epoch: {}'.format(total_dataload_time, total_train_time, epoch_time))
+
+        data_loading_time.append(total_dataload_time)
+        # print('Epoch [%d/%d]' % (epoch + 1, epochs))
+        # print('Total DataLoading Time: ', total_dataload_time)
+        training_time.append(total_train_time)
+        total_epoch_time.append(epoch_time)
 
         network.eval()
         with torch.no_grad():
@@ -169,6 +229,28 @@ def main():
                 print('Epoch [%d/%d] %s validation Loss: %.4f, Accuracy: %.4f' % (
                     epoch + 1, epochs, name, total_validation_loss / (j + 1), accuracy))
 
+    
+    data_loading_time = np.array(data_loading_time)
+    training_time = np.array(training_time)
+    total_epoch_time = np.array(total_epoch_time)
+
+    print()
+    print('Avergae Data Loading Time per Epoch: ', np.mean(data_loading_time))
+    print('Avergae Training Time per Epoch: ', np.mean(training_time))
+    print('Avergae Time per Epoch: ', np.mean(total_epoch_time))
+
+    np.save('/home/qg2205/HPML-project/data_loading_time' + str(args.n) + '.npy', data_loading_time)
+    np.save('/home/qg2205/HPML-project/training_time.npy', training_time)
+    np.save('/home/qg2205/HPML-project/total_epoch_time.npy', total_epoch_time)
+
+
+    # print(prof.key_averages(group_by_input_shape=True).table(sort_by="cpu_time_total", row_limit=10))
+    # print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
+    # print(prof.key_averages().table(sort_by="cpu_memory_usage", row_limit=10))
+    # print(prof.key_averages().table(sort_by="cuda_memory_usage", row_limit=10))
+
+    # prof.export_chrome_trace("/home/qg2205/HPML-project/src")
+    
 
 if __name__ == "__main__":
     main()
